cee79455c5f834cf9c3d160d9e80b554bbaa2b48
Th3FatGuy <minhvk1609@gmail.com>
2025-06-24 10:48:06 +0700

    Add API to code

--- assets/js/function.js
+++ assets/js/function.js
@@ -3,40 +3,45 @@
 // let tasks = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
 
 let taskList = [];
+let taskListSearch = [];
 let url = mockapi.baseURL; 
-
-getTasks = fetch(url, {
-  method: 'GET',
-  headers: {'content-type':'application/json'},
-  }).then(res => {
-    if (res.ok) {
-        return res.json();
-    }
-}).then(tasks => {
-  taskList = tasks;
-  renderTasks();
-});
 
 const taskTableBody = document.querySelector(".taskTable__body");
 
-// // Add event listener to the add button
-// document.getElementsByClassName("table__btn__addTask")[0].addEventListener("click", () => {
-//   const name = prompt("Enter task name:");
-//   if (!name) return;
-  
-//   tasks.push({
-//     "name": name,
-//     "status": "toDo"
-//   });
+// Add event listener to the add button
+document.getElementsByClassName("table__btn__addTask")[0].addEventListener("click", () => {
+  const name = prompt("Enter task name:");
+  newTask = {
+    name: name,
+    status: "todo"
+  }
+  if (!name) {
+    alert("Task name cannot be empty.");
+    return;
+  } else if (name in taskList.map(task => task.name)) {
+    alert("Task name already exists.");
+    return;
+  } else{
+    fetch(url, {
+      method: 'POST',
+      headers: {'content-type':'application/json'},
+      body: JSON.stringify(newTask)
+    }).then(res => {
+      if (res.ok) {
+          return res.json();
+      }
+    }).then(task => {
+      taskList.push(task);
+      renderTasks(taskList);
+    }).catch(error => {
+      console.log("Error adding task:", error);
+    })
+  }
+});
 
-//   saveTasks();
-//   renderTasks();
-// });
-
-function renderTasks() {
+function renderTasks(taskListToRender) {
   taskTableBody.innerHTML = "";
-  console.log(taskList);
-  taskList.forEach((task) => {
+  taskListToRender.forEach(task => {
     const row = document.createElement("tr");
     
     row.innerHTML = `
@@ -44,8 +49,8 @@ function renderTasks() {
       <td class="taskTable__body__name">${task.name}</td>
       <td class="taskTable__body__status">
         <select onchange="updateStatus(${task.id}, this.value)">
-          <option value="toDo" ${task.status === 'todo' ? 'selected' : ''}>To Do</option>
-          <option value="inProcess" ${task.status === 'inprocess' ? 'selected' : ''}>In Process</option>
+          <option value="todo" ${task.status === 'todo' ? 'selected' : ''}>To Do</option>
+          <option value="inprocess" ${task.status === 'inprocess' ? 'selected' : ''}>In Process</option>
           <option value="completed" ${task.status === 'completed' ? 'selected' : ''}>Completed</option>
         </select>
       </td>
@@ -72,51 +77,79 @@ document.getElementsByClassName("table__search__btn")[0].addEventListener("click
         return res.json();
     }
   }).then(tasks => {
-    taskList = tasks;
-    renderTasks();
+    taskListSearch = tasks;
+    renderTasks(taskListSearch);
   }).catch(error => {
     console.log("Error fetching tasks:", error);
   })    
 });
 
 
-// function updateStatus(id, newStatus) {
-//   taskList[index].status = newStatus;
-//   saveTasks();
-// }
-
-// function editTask(index) {
-//   const taskNames = Object.keys(tasks);
-//   const taskNameToEdit = taskNames[index];
-//   const newName = prompt("Enter new task name:", taskNameToEdit);
-
-//   if (newName) {
-//     tasks[index].name = newName;
-//     saveTasks();
-//     renderTasks();
-//   }
-// }
-
-// function removeTask(index) {
-//   tasks.pop(index);
-//   saveTasks();
-//   renderTasks();
-// }
-
-function saveTasks() {
-  
+function updateStatus(id, newStatus) {
+  let editURL = `${url}/${id}`;
+  fetch(editURL, {
+    method: 'PUT',
+    headers: {'content-type':'application/json'},
+    body: JSON.stringify({status: newStatus})
+  }).then(res => {
+    if (res.ok) {
+        return res.json();
+    }
+  }).catch(error => {
+    console.log("Error updating task status:", error);
+  })
 }
 
-// function searchTask(value){
-//   // filter tasks based on the search value
-//   const filteredTasks = tasks.filter(task => task.name.toLowerCase().includes(value.toLowerCase()));
-//   // render the filtered tasks
-//   renderNewTasks(filteredTasks);
-// }
+function editTask(index) {
+  let taskNameToEdit = taskList[index].name;
+  const newName = prompt("Enter new task name:", taskNameToEdit);
+  let editURL = `${url}/${index}`;
 
-// function clearSearch() {
-//   document.querySelector(".table__search__input").value = "";
-//   renderTasks();
-// }
+  fetch(editURL, {
+    method: 'PUT',
+    headers: {'content-type':'application/json'},
+    body: JSON.stringify({name: newName})
+  }).then(res => {
+    if (res.ok) {
+        return res.json();
+    }
+  }).then(updatedTask => {
+    taskList = taskList.map(task => task.id === updatedTask.id ? updatedTask :task);
+    renderTasks(taskList);
+  }).catch(error => {
+    console.log("Error updating task status:", error);
+  })
+}
 
-window.onload = renderTasks;
+function removeTask(index) {
+  let deleteURL = `${url}/${index}`;
+  fetch(deleteURL, {
+    method: 'DELETE',
+  }).then(res => {
+    if (res.ok) {
+        return res.json();
+    }
+  }).then(task => {
+    taskList = taskList.filter(t => t.id !== task.id);
+    renderTasks(taskList);
+  }).catch(error => {
+    console.log("Error deleting task:", error);
+  })
+}
+
+function clearSearch() {
+  document.querySelector(".table__search__input").value = "";
+  renderTasks(taskList);
+}
+
+window.onload = fetch(url, {
+    method: 'GET',
+    headers: {'content-type':'application/json'},
+      }).then(res => {
+        if (res.ok) {
+            return res.json();
+        }
+    }).then(tasks => {
+      taskList = tasks;
+      renderTasks(taskList);
+  });
